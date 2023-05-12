ffbab8fe0544c03b35dbed2d98e1cbcba15cd034
soxhwhat <1246669436@qq.com>
2023-05-12 23:31:19 +0800

    docs: 声明式渲染
    能在改变时触发更新的状态被认为是响应式的。在 Vue 中，响应式状态被保存在组件中。
    我们可以使用 data
    组件选项来声明响应式状态，该选项应该是一个返回对象的函数：message 属性可以在模板中使用。下面展示了我们如何使用双花括号法，根据
    message 的值来渲染动态文本：
    <h1>{{ message
    }}</h1>
    
    在双花括号中的内容并不只限于标识符或路径——我们可以使用任何有效的 JavaScript 表达式。
    <h1>{{
    message.split('').reverse().join('') }}</h1>

---
+++ src/quickStart/Start.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello Vue 3 + Vite',
+            counter : {
+                count : 0
+            }
+        }
+    }
+
+}
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <p>Count is : {{ counter.count }}</p>
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+</template>
+
+<style scoped>
+
+</style>
