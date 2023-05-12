34d6cdb65a7db21d1cae6c65cc93c7a234704a84
soxhwhat <1246669436@qq.com>
2023-05-12 23:36:58 +0800

    docs: Attribute 绑定
    :class=""是Vue.js中的动态class绑定语法，用于根据Vue实例中的数据动态修改元素的CSS类。它可以接受一个表达式作为参数，并返回一个对象，对象的属性名是要添加的CSS类名，属性值是一个布尔值，表示该类名是否应该被添加到元素上。
    
    上述代码中，我们在<div>标签上使用了:class指令，并传递了一个对象作为参数。该对象的属性名分别为active和text-danger，它们对应着我们要添加的CSS类名；而属性值则分别为isActive和error，它们对应着Vue实例中的数据属性。这样，当isActive为true时，active类会被添加到<div>元素上；当error为true时，text-danger类会被添加到<div>元素上

---
+++ src/quickStart/Attribute.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            isActive: true,
+            error: false
+        }
+    }
+}
+</script>
+
+<template>
+    <div :class="{ active: isActive, 'text-danger': error }"></div>
+
+</template>
+
+<style>
+.text-danger {
+    color: red;
+}
+
+.active {
+    font-weight: bold;
+}
+
+</style>
