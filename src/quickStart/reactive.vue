f7a217cd48dd8606767ee8d28bdf7c3e242c6c05
soxhwhat <1246669436@qq.com>
2023-05-13 13:47:24 +0800

    docs: ref函数入门
    1.reactive和ref函数的共同作用是什么?
     用函数调用的方式生成响应式数据
    2. reactive vs ref ?
        1.reactive不能处理简单类型的数据
        2.ref参数类型支持更好但是必须通过value访问修改
        3.ref函数的内部实现依赖于reactive函数
    3.在实际工作中推荐使用哪个?
     推荐使用ref函数，更加灵活，小兔鲜项目主用ref

---
+++ src/quickStart/reactive.vue
@@ -0,0 +1,26 @@
+<script setup>
+// 1. 导入函数
+import { reactive} from "vue";
+
+// 2. 执行函数， 传入一个对象类型的参数 变量接受返回值,注意返回值也是一个对象
+// 不用reactive的话，state.count是不会响应式的，变量的值不会变化
+const state = reactive({
+    count: 0
+})
+
+const setState = () => {
+    state.count++
+}
+</script>
+
+<template>
+    <div>
+        <h1>Count: {{ state.count }}</h1>
+        <button @click="setState">Increment</button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/quickStart/ref.vue
+++ src/quickStart/ref.vue
@@ -1,11 +1,21 @@
-<script>
-export default {
-    mounted() {
-        this.$refs.p.textContent = 'mounted!'
-    }
+<script setup>
+//1. 导入ref对象
+import { ref } from "vue";
+
+//2. 执行函数， 传入一个参数(可以是简单类型或者对象），变量接受返回值
+const count = ref(0)
+
+const increment = () => {
+    //采用.value的方式获取值
+    count.value++
 }
 </script>
 
 <template>
-    <p ref="p">hello</p>
+  <p>count is: {{ count }}</p>
+  <button @click="increment">Increment</button>
 </template>
+
+<style scoped>
+
+</style>
