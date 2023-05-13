dc2a3094e60469583491e9caf77d9911711ed578
soxhwhat <1246669436@qq.com>
2023-05-13 09:58:12 +0800

    docs: 列表渲染
    我们可以使用 v-for 指令来渲染一个基于源数组的列表：
    <ul>
      <li v-for="todo in todos" :key="todo.id">
        {{ todo.text }}
      </li>
    </ul>
    注意，我们还给每个 todo 对象设置了唯一的 id，并且将它作为特殊的 key attribute 绑定到每个 <li>。key 使得 Vue 能够精确的移动每个 <li>，以匹配对应的对象在数组中的位置。
    
    更新列表有两种方式：
    1.在原数组上调用变更方法 this.todos.push(newTodo)
    2.使用新的数组替代原数组
    this.todos = this.todos.filter(/* ... */)

---
+++ src/quickStart/ulRender.vue
@@ -0,0 +1,58 @@
+<script>
+let id = 0
+
+export default {
+    data() {
+        return {
+            newTodo: '',
+            todos: [
+                {
+                    id: id++,
+                    text: 'Learn Vue.js',
+                    done: false
+                },
+                {
+                    id: id++,
+                    text: 'Learn React.js',
+                    done: false
+                },
+                {
+                    id: id++,
+                    text: 'Learn Angular.js',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({
+                id: id++,
+                text: this.newTodo,
+                done: false
+            })
+        },
+        removeTodo(todo) {
+            this.todos.pop(todo)
+
+        }
+    }
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+      <input v-model="newTodo">
+      <button>Add Todo</button>
+  </form>
+  <ul>
+      <li v-for="todo in todos" :key="todo.id">
+          <span>{{ todo.text }}</span>
+          <button @click="removeTodo(todo)">Remove</button>
+      </li>
+  </ul>
+</template>
+
+<style>
+
+</style>
