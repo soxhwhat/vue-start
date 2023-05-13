579ee37b558b7407c269b155a516844fc0d6140e
soxhwhat <1246669436@qq.com>
2023-05-13 17:10:11 +0800

    docs: 顶层和底层组件通信
    1.provide和inject的作用是什么?
    跨层组件通信
    2.如何在传递的过程中保持数据响应式?
    第二个参数传递ref对象
    3.底层组件想要通知顶层组件做修改，如何做?
    传递方法，底层组件调用方法
    4.一颗组件树中只有一个顶层或底层组件吗?
    相对概念，存在多个顶层和底层的关系

---
+++ src/quickStart/room-msg-comment.vue
@@ -0,0 +1,26 @@
+<script setup>
+// 2. 接收数据
+import {inject} from "vue";
+
+const roomData = inject('data-key')
+
+// 接收响应式数据
+const data = inject('response-key')
+
+
+// 接收方法
+const change = inject('change-key')
+</script>
+
+<template>
+  <div>
+      <h1>我是子组件</h1>
+      <p>来自顶层组件的数据：{{ roomData }}</p>
+      <p>来自顶层组件的响应式数据：{{ data }}</p>
+      <button @click="change(100)">使用方法修改顶层的数据</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/quickStart/room-page.vue
@@ -0,0 +1,30 @@
+<script setup >
+import {provide, ref} from "vue";
+//1. 顶层组件提供数据
+provide('data-key', '我是父组件的数据')
+
+// 传递响应式数据
+const data = ref(0)
+provide('response-key', data)
+setTimeout(()=>{
+    data.value = 100
+}, 3000)
+
+
+//传递方法
+const change = (value) => {
+    console.log('父组件接收到子组件传递的值', value)
+}
+provide('change-key', change)
+</script>
+
+<template>
+  <div>
+      <h1>我是父组件</h1>
+    <child-comp></child-comp>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
