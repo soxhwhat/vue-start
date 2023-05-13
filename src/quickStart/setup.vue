9a58b82586eecf83c8227b97e2fd820388fd365b
soxhwhat <1246669436@qq.com>
2023-05-13 13:34:12 +0800

    docs: setup函数入门
    1.setup选项的执行时机?
    beforeCreate钩子之前 自动执行
    2.setup写代码的特点是什么?
    定义数据+函数
    然后以对象方式return
    3.<script setup>解决了什么问题?
    经过语法糖的封装更简单的使用组合式API
    4.setup中的this还指向组件实例吗?指向undefined

---
+++ src/quickStart/setup.vue
@@ -0,0 +1,38 @@
+<!--<script>-->
+<!--  export default {-->
+<!--      setup() {-->
+<!--          console.log('setup')-->
+<!--          const message = 'Hello World'-->
+<!--          const logMessage = () => {-->
+<!--              console.log(message)-->
+<!--          }-->
+<!--          return {-->
+<!--              message,-->
+<!--              logMessage-->
+<!--          }-->
+<!--      },-->
+<!--      beforeCreate() {-->
+<!--          console.log('beforeCreate')-->
+<!--      }-->
+<!--  }-->
+<!--</script>-->
+
+<!--  setup语法糖优化-->
+<script setup>
+    console.log('setup')
+    const message = 'Hello World'
+    const logMessage = () => {
+        console.log(message)
+    }
+</script>
+
+<template>
+  <div>
+      {{ message }}
+      <button @click="logMessage">Log Message</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
