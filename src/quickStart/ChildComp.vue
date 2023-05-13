8239e66df00ce01656093f29f1ec539ff034c560
soxhwhat <1246669436@qq.com>
2023-05-13 15:04:53 +0800

    docs: 父子之间传递数据
    父传子
    1.父传子的过程中通过什么方式接收props?
    	defineProps({ 属性名: 类型
    })
    2.setup语法糖中如何使用父组件传过来的数据?
    	const props = defineProps([
    属性名:类型])
    
    子传父
    1.子传父的过程中通过什么方式得到emit方法?
    	defineEmits(['事件名称])

--- src/quickStart/ChildComp.vue
+++ src/quickStart/ChildComp.vue
@@ -1,3 +1,21 @@
+<script setup>
+//2. 接受父组件传递的props
+const props = defineProps({
+    msg: String
+})
+// 通过defineEmits定义事件emit方法
+const emit = defineEmits(['change'])
+
+const sendMsg = () => {
+    emit('change', 200)
+}
+</script>
+
 <template>
-    <slot>Fallback content</slot>
+    <div>
+        <h1>子组件</h1>
+        <p>{{ props.msg }}</p>
+    </div>
+<!--    子传父-->
+    <button @click="sendMsg">子传父</button>
 </template>
--- src/quickStart/Component.vue
+++ src/quickStart/Component.vue
@@ -1,18 +1,22 @@
-<script>
+<script setup>
+// 引入子组件
 import ChildComp from './ChildComp.vue'
+import {ref} from "vue";
 
-export default {
-    components: {
-        ChildComp
-    },
-    data() {
-        return {
-            msg: 'from parent'
-        }
-    }
+const msg = ref(100)
+setTimeout(() => {
+    msg.value = 200
+}, 2000)
+
+const change = (value) => {
+    console.log('父组件接收到子组件传递的值', value)
 }
 </script>
 
 <template>
-    <ChildComp>Message: {{ msg }}</ChildComp>
+<!--    绑定属性msg-->
+    <ChildComp :msg="msg"></ChildComp>
+
+<!--    子传父-->
+    <ChildComp @change="change"></ChildComp>
 </template>
