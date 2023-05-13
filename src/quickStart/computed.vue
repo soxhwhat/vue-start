88905b41cea1d0095207d8c3d2928d16ab115191
soxhwhat <1246669436@qq.com>
2023-05-13 13:54:28 +0800

    docs: 计算函数基本用法
    最佳实践：
    1. 计算属性不应该有副作用
      比如异步请求/修改dom
    
    2.避免直接修改属性的值
      计算属性应该是只读的

---
+++ src/quickStart/computed.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {computed, ref} from "vue";
+
+  const list = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+  const computedList = computed(() => {
+      return list.value.filter(item => item % 2 === 0)
+  })
+
+</script>
+
+<template>
+  <div>
+      原始列表：{{ list }}
+      <br>
+      计算列表：{{ computedList }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
