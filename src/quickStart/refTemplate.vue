4d213cc333e03c160ec61618a9a35a7d4b0f7b95
soxhwhat <1246669436@qq.com>
2023-05-13 14:44:23 +0800

    docs: 生命周期函数

--- src/quickStart/refTemplate.vue
+++ src/quickStart/refTemplate.vue
@@ -1,9 +1,14 @@
-<script>
-export default {
-    mounted() {
-        this.$refs.p.textContent = 'mounted!'
-    }
-}
+<script setup>
+import { onMounted} from "vue";
+
+onMounted(() => {
+    console.log('组件挂载完毕mounted执行了')
+})
+
+// 生命周期函数可以执行多次，多次执行的时候，会在时机成熟的时候依次执行
+onMounted(() => {
+    console.log('组件更新完毕mounted执行了')
+})
 </script>
 
 <template>
