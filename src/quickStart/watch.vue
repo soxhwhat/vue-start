6e9326ca01bbe8500aa0fb28d2d1d388fa4db1f3
soxhwhat <1246669436@qq.com>
2023-05-13 14:36:33 +0800

    docs: watch监听函数基本用法
    1.作为watch函数的第一个参数，ref对象需要添加.value吗?
    不需要，watch会自动读取2.watch只能侦听单个数据吗?
    单个或者多个
    3.不开启deep，直接修改嵌套属性能触发回调吗?
    不能，默认是浅层侦听
    4.不开启deep，想在某个层次比较深的属性变化时执行回调怎么做?可以把第一个参数写成函数的写法，返回要监听的具体属性

--- src/quickStart/watch.vue
+++ src/quickStart/watch.vue
@@ -1,34 +1,60 @@
-<script>
-export default {
-    data() {
-        return {
-            todoId: 1,
-            todoData: null
-        }
-    },
-    methods: {
-        async fetchData() {
-            this.todoData = null
-            const res = await fetch(
-                `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
-            )
-            this.todoData = await res.json()
-        }
-    },
-    mounted() {
-        this.fetchData()
-    },
-    watch: {
-        todoId() {
-            this.fetchData()
-        }
-    }
+<script setup>
+import { ref, watch } from 'vue'
+
+const count = ref(0)
+const setCount = () => {
+    count.value++
 }
+
+const name = ref('sam')
+const setName = () => {
+    name.value = 'jack'
+}
+
+const state = ref({
+    count: 0,
+    name: 'sam'
+})
+// watch监听count的变化(单数据源）
+watch(count, (value, oldValue) => {
+    console.log('count changed', value, oldValue)
+})
+
+// watch监听count的变化(多数据源）
+watch([count, name], ([countValue, nameValue], [countOldValue, nameOldValue]) => {
+    console.log('count changed', countValue, countOldValue)
+    console.log('name changed', nameValue, nameOldValue)
+})
+
+// immediate: 在侦听器创建时立即执行回调
+watch(count, (value, oldValue) => {
+    console.log('count changed', value, oldValue)
+}, { immediate: true })
+
+//deep: 通过watch监听的ref对象默认是浅层侦听，直接修改嵌套的对象属性是不会触发回调的，需要开启deep
+watch(state, ()=> {
+    console.log('state changed')
+}, { deep: true })
+
+// 精准侦听对象的某个属性，此处当state.count变化时，回调才会执行
+watch(() => state.value.count, (value, oldValue) => {
+    console.log('state.count changed', value, oldValue)
+})
 </script>
 
 <template>
-    <p>Todo id: {{ todoId }}</p>
-    <button @click="todoId++">Fetch next todo</button>
-    <p v-if="!todoData">Loading...</p>
-    <pre v-else>{{ todoData }}</pre>
+    <div>
+        <h1>Count: {{ count }}</h1>
+        <button @click="setCount">Increment</button>
+    </div>
+
+    <div>
+        <h1>Name: {{ name }}</h1>
+        <button @click="setName">setName</button>
+    </div>
+
+    <div>
+        <h1>State: {{ state }}</h1>
+        <button @click="state.count++">setState</button>
+    </div>
 </template>
